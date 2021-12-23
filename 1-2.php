b14d8a3d8b9a9f4693b9cdc4dd38117ab68248d1
blackstarss <kota.weeds1204@gmail.com>
2021-12-23 19:06:53 +0900

    [start]chapter

---
+++ 1-2.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <?php
+    $jojo = 'ジョナサン・ジョースター';
+    $jonathan = 'ジョナサン・ジョースター';
+    $joester_san = 'ジョナサン・ジョースター';
+    echo $jojo;
+    echo '<br>';
+    echo $jonathan;
+    echo '<br>';
+    echo $joester_san;
+    echo '<br>';
+    echo 'ここからは第二部<br>';
+    $jojo = 'ジョセフ・ジョースター';
+    echo $jojo;
+    echo '<br>';
+    echo $jonathan;
+    echo '<br>';
+    $ojiichan = $jonathan;
+    echo $ojiichan;
+    echo 'ここからは第三部';
+    echo
+    
+    echo '<br>';
+    $jojo = '空条承太郎'
+  ?>
+</body>
+</html>
